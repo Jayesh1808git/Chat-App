7d85ba5304f4ea58f83d0bbae28b179ef0304007
Jayesh1808git <jayeshgund2005@gmail.com>
2025-04-02 22:35:36 +0530

    Chat container fixed

--- Frontend/src/Components/ChatContainer.jsx
+++ Frontend/src/Components/ChatContainer.jsx
@@ -18,7 +18,7 @@ const ChatContainer = () => {
   } = useChatStore();
   const { authUser } = useAuthStore();
   const messageEndRef = useRef(null);
-
+ 
   useEffect(() => {
     getMessages(selectedUser._id);
 
@@ -59,8 +59,8 @@ const ChatContainer = () => {
                 <img
                   src={
                     message.senderId === authUser._id
-                      ? authUser.profilePic || "/avatar.png"
-                      : selectedUser.profilePic || "/avatar.png"
+                      ? authUser.profilepic || "/avatar.png"
+                      : selectedUser.profilepic || "/avatar.png"
                   }
                   alt="profile pic"
                 />
